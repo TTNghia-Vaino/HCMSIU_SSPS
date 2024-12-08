e98e72e83a5f64f4f39a1b9888fa939146420707
Vaino <tatrongnghiak15@siu.edu.vn>
2024-12-08 15:35:41 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HCMSIU_SSPS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
