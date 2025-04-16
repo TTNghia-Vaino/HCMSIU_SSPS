6c4b1111e032e506948f96d2d493d3daeb4ff065
xuancanh <bxcanhvlog@gmail.com>
2025-04-16 22:43:54 +0700

    updat fontend

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 ﻿using HCMSIU_SSPS.Models;
 using HCMSIU_SSPS.Services;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<HcmsiuSspsContext>();
+builder.Services.AddDbContext<HcmsiuSspsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddScoped<IEmailService, EmailService>();
 
 // Cấu hình giới hạn tải file
---
+++ Views/Home/Profile.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HCMSIU_SSPS.Views.Home
+{
+    public class ProfileModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
