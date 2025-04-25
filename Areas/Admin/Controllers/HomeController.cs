8f99f77e306dc4e288408edd4e64ccbec50a06d2
Vaino <ttnghia004@gmail.com>
2025-04-25 16:10:21 +0700

    update 4:10PM - wait for VN ver

--- Areas/Admin/Controllers/HomeController.cs
+++ Areas/Admin/Controllers/HomeController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using HCMSIU_SSPS.Models; // Sửa lại namespace nếu cần
+using System.Linq;
 
 namespace HCMSIU_SSPS.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
-        [Area("Admin")]
+        private readonly HcmsiuSspsContext _context;
+
+        public HomeController(HcmsiuSspsContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var totalUser = _context.Users.Count();
+            var totalPrinter = _context.Printers.Count(p => p.IsEnable == 1);
+            var totalPrintJob = _context.PrintJobs.Count(j => j.Status == 0);
+
+            ViewBag.TotalUser = totalUser;
+            ViewBag.TotalPrinter = totalPrinter;
+            ViewBag.TotalPrintJob = totalPrintJob;
+
             return View();
         }
     }
